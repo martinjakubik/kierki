bf320311282ec5e27b9b69e7f4ada8683665effd
martin jakubik <martin.jakubik@gmail.com>
2025-06-14 15:13:51 +0200

    ran code on page

---
+++ app/harness.js
@@ -0,0 +1,6 @@
+const CSS_CLASS_SHOW_FACE='showFace';
+const CSS_CLASS_SHOW_BACK='showBack';
+
+const oCard = document.getElementById('card-kierki-ad');
+oCard.classList.remove(CSS_CLASS_SHOW_FACE);
+oCard.classList.add(CSS_CLASS_SHOW_BACK);
