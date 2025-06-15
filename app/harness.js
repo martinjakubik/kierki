7c8579df9190a946a8fc0e51f30c88868ceb04ff
martin jakubik <martin.jakubik@gmail.com>
2025-06-15 17:02:39 +0200

    added click to flip

--- app/harness.js
+++ app/harness.js
@@ -2,5 +2,26 @@ const CSS_CLASS_SHOW_FACE='showFace';
 const CSS_CLASS_SHOW_BACK='showBack';
 
 const oCard = document.getElementById('card-kierki-ad');
-oCard.classList.remove(CSS_CLASS_SHOW_FACE);
-oCard.classList.add(CSS_CLASS_SHOW_BACK);
+
+function flipCard (sCardId) {
+    const oCard = document.getElementById(sCardId);
+    if (oCard.classList.contains(CSS_CLASS_SHOW_FACE)) {
+        oCard.classList.remove(CSS_CLASS_SHOW_FACE);
+        oCard.classList.add(CSS_CLASS_SHOW_BACK);
+    } else {
+        oCard.classList.remove(CSS_CLASS_SHOW_BACK);
+        oCard.classList.add(CSS_CLASS_SHOW_FACE)
+    }
+}
+
+function handleCardTapped (oEvent) {
+    const oTarget = oEvent.currentTarget;
+    const sCardId = oTarget.id;
+    flipCard (sCardId);
+}
+
+const sCardClass = 'card';
+const aCards = document.getElementsByClassName(sCardClass);
+for (const oCard of aCards) {
+    oCard.onclick = handleCardTapped;
+};
